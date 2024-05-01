ece055c9c63d3773fd7b09acc9059be093e7faa6
baldjon <162023500+baldjon@users.noreply.github.com>
2024-05-01 20:49:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 function showSection(sectionId) {
-    const sections = document.querySelectorAll('.content-section');
-    sections.forEach(section => {
-        if (section.id === sectionId) {
-            section.style.display = 'block';
-        } else {
-            section.style.display = 'none';
-        }
-    });
+    var sections = document.getElementsByClassName('content-section');
+    for (var i = 0; i < sections.length; i++) {
+        sections[i].style.display = 'none'; // Versteckt alle Abschnitte
+    }
+    document.getElementById(sectionId).style.display = 'block'; // Zeigt den gewählten Abschnitt an
 }
-
